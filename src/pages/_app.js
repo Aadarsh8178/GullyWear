3b71dedd0b109bccecd273ec320d8d5a50fc9692
Aadarsh <vaadarsh8178@gmail.com>
2020-09-15 13:31:32 +0530

    favIcon/page-title-added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,78 +1,83 @@
-import React from 'react';
-import App from 'next/app';
-import Router from 'next/router'
-import Navbar from '../components/Navigation/Navbar'
-import {LookProvider} from '../store/context'
-import { PageTransition } from 'next-page-transitions'
+import React from "react";
+import App from "next/app";
+import Router from "next/router";
+import Navbar from "../components/Navigation/Navbar";
+import { LookProvider } from "../store/context";
+import { PageTransition } from "next-page-transitions";
 
-Router.onRouteChangeStart = url => {
-  document.getElementById('loading').style.display = 'inline'
-}
-Router.onRouteChangeComplete = () => document.getElementById('loading').style.display = 'none';
-Router.onRouteChangeError = () => document.getElementById('loading').style.display = 'none';
+Router.onRouteChangeStart = (url) => {
+  document.getElementById("loading").style.display = "inline";
+};
+Router.onRouteChangeComplete = () =>
+  (document.getElementById("loading").style.display = "none");
+Router.onRouteChangeError = () =>
+  (document.getElementById("loading").style.display = "none");
 
 class MyApp extends App {
+  componentDidMount() {
+    document.title = "Gully Wear";
+  }
   render() {
-    const { Component, pageProps,router } = this.props;
+    const { Component, pageProps, router } = this.props;
 
     return (
       <LookProvider>
-        <Navbar/>
+        <Navbar />
         <PageTransition timeout={300} classNames="page-transition">
-          <Component {...pageProps} key={router.route}/>
-        </PageTransition> 
-    <style jsx global>{`
-    * {
-      margin: 0;
-      padding: 0;
-      box-sizing: border-box;
-    }
-    /* globals */
-    body {
-      padding-top:85px;
-      color: #222;
-      background: rgb(250, 249, 248);
-      font-family: Verdana, Geneva, Tahoma, sans-serif;
-      line-height: 1.4;
-    }
-    .page-transition-enter {
-      transform:translateY(10%);
-    }
-    .page-transition-enter-active {
-      transform:translateY(0);
-      transition: all 300ms;
-    }
-    .page-transition-exit {
-      opacity:0;
-    }
-    .page-transition-exit-active {
-      opacity:0;
-      transition: all 0;
-    }
-    .btn-primary {
-      display: inline-block;
-      text-decoration: none;
-      background:transparent;
-      color:white;
-      font-family: 'Open Sans Condensed', sans-serif;
-      font-size:11px;
-      padding: 0.5rem 1.5rem;
-      border: white 2px solid;
-      transition: all 0.3s linear;
-      text-transform: uppercase;
-      cursor: pointer;
-    }
-    .btn-primary:hover {
-      
-      font-weight:bold;
-    }
-    @media screen and (max-width:768px){
-      body{
-        padding-top:58px;
-      }
-    }
-  `}</style>
-      </LookProvider>)
+          <Component {...pageProps} key={router.route} />
+        </PageTransition>
+        <style jsx global>{`
+          * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+          }
+          /* globals */
+          body {
+            padding-top: 85px;
+            color: #222;
+            background: rgb(250, 249, 248);
+            font-family: Verdana, Geneva, Tahoma, sans-serif;
+            line-height: 1.4;
+          }
+          .page-transition-enter {
+            transform: translateY(10%);
+          }
+          .page-transition-enter-active {
+            transform: translateY(0);
+            transition: all 300ms;
+          }
+          .page-transition-exit {
+            opacity: 0;
+          }
+          .page-transition-exit-active {
+            opacity: 0;
+            transition: all 0;
+          }
+          .btn-primary {
+            display: inline-block;
+            text-decoration: none;
+            background: transparent;
+            color: white;
+            font-family: "Open Sans Condensed", sans-serif;
+            font-size: 11px;
+            padding: 0.5rem 1.5rem;
+            border: white 2px solid;
+            transition: all 0.3s linear;
+            text-transform: uppercase;
+            cursor: pointer;
+          }
+          .btn-primary:hover {
+            font-weight: bold;
+          }
+          @media screen and (max-width: 768px) {
+            body {
+              padding-top: 58px;
+            }
+          }
+        `}</style>
+      </LookProvider>
+    );
   }
 }
 
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,28 +1,37 @@
-import Document,{Head,Main,NextScript} from 'next/document';
+import Document, { Head, Main, NextScript } from "next/document";
 
-export default class MyDocument extends Document{
-    render(){
-        return(
-            <html lang="en">
-                <Head>
-                    <meta name="description" content="A clothing site"/>
-                    <meta charSet="utf-8"/>
-                    <meta name="robots" content="noindex, nofollow"/>
-                    <meta name="viewport" content="width=device-width"/>
-                    <link rel="stylesheet"
-                    href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"/>
-                    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet"></link>
-                    <link href="https://fonts.googleapis.com/css?family=Cinzel&display=swap" rel="stylesheet"></link>
-                    <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet"/>
-                    <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:700&display=swap" rel="stylesheet"></link>
-                </Head>
-                <body>
-                    <Main/>
-                    <NextScript/>
-                </body>
-                
-            </html>
-        )
-    }
-    
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <html lang="en">
+        <Head>
+          <meta name="description" content="A clothing site" />
+          <meta charSet="utf-8" />
+          <meta name="robots" content="noindex, nofollow" />
+          <meta name="viewport" content="width=device-width" />
+          <link rel="shortcut icon" href="/static/favicon.ico" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap"
+            rel="stylesheet"
+          ></link>
+          <link
+            href="https://fonts.googleapis.com/css?family=Cinzel&display=swap"
+            rel="stylesheet"
+          ></link>
+          <link
+            href="https://fonts.googleapis.com/css?family=Lato&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:700&display=swap"
+            rel="stylesheet"
+          ></link>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
 }
